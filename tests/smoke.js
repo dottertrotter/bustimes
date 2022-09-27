4d33b850799084dc4ef47b82b4a2d111a63284b1
Brad Marsh <bradleyt.marsh@gmail.com>
2022-09-27 09:29:09 -0500

    added tests for navigation buttons

--- tests/smoke.js
+++ tests/smoke.js
@@ -45,3 +45,27 @@ describe('Stop Results Page', function() {
       .end();
   });
 });
+
+describe('Navigation', function() {
+  //http://localhost:3000/991/direction/1/stop/MAAM
+
+  //http://localhost:3000/991/direction/0/stop/HHTE
+
+  it('The user should be able to navigate with the back and forward browser buttons', function(browser) {
+    browser
+      .url('http://localhost:3000/991/direction/0/stop/HHTE')
+      .waitForElementVisible('body')
+      .assert.textContains("#stop-reference", "57188")
+      .url('http://localhost:3000/991/direction/1/stop/MAAM')
+      .waitForElementVisible('body')
+      .assert.textContains("#stop-reference", "56881")
+      .back()
+      .waitForElementVisible('body')
+      .assert.textContains("#stop-reference", "57188")
+      .forward()
+      .waitForElementVisible('body')
+      .assert.textContains("#stop-reference", "56881")
+      .end();
+  });
+});
+
